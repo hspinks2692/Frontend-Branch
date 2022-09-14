a5ee8c1eb7b500235ed9048f372ff56bb32407bd
hspinks2692 <hspinks2692@yahoo.com>
2022-09-14 16:49:23 -0400

    Added info to Home Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,14 @@ function Footer(props) {
     padding: "8px",
     width: "90%",
     margin: "auto",
+    backgroundColor: "rgb(139, 215, 233)"
   };
 
   return (
     <footer>
       <nav style={navStyle}>
-        <Link to="/">
-          <div>HOME</div>
-        </Link>
-        <Link to="/about">
-          <div>ABOUT</div>
-        </Link>
+        <a href="https://github.com/hspinks2692">Github</a>
+        <a href="https://docs.google.com/document/d/1DrI6AP4276d1ef2a0yyRN3om3PCGP_qM/">Resume</a>
         <Link to="/projects">
           <div>PROJECTS</div>
         </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <div>
+          <img src="https://i.imgur.com/k7M7Scb.png" alt="Harvey Spinks"></img>
+        </div>
+        <p>Hello and thank you for visiting my porfolio! Here you can see a lot of my previous projects as well as a lot of information about me. Here I'll list all languages I know as well as a link to my resume! Here's a list of all languages I know and can use:</p>
+        <ul>
+          <li>HTML/CSS/Javascript</li>
+          <li>Express</li>
+          <li>React</li>
+          <li>Mongoose</li>
+          <li>C#/Unity</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>Visual Basic</li>
+        </ul>
+        <p>Thank you for visiting my portfolio! I hope you find me a great fit for your team!</p>
+      </div>
+    );
   }
   
   export default Home;
